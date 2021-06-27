6e14dec6a72deac6ce0ef7779f99ff5558f495ad
Feranmi Akinlade <30551780+uxFeranmi@users.noreply.github.com>
2021-06-27 23:02:50 +0200

    Fix redirect

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/published-elsewhere');
+  });
+
 	return <>
 		<h2 className="title">
-			Coming <a href="https://nextjs.org">soon!</a>
+			Coming soon!
+			<br/>
+			<small>
+				<a href="/published-elsewhere">View previous articles.</a>
+			</small>
 		</h2>
 	</>
 };
