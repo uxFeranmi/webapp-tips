4e5c76452b9ac02d4ad7904d34ed24fd7acf6c61
Feranmi Akinlade <30551780+uxFeranmi@users.noreply.github.com>
2022-05-17 18:50:15 +0100

    Check credential helper settings

--- index.test.js
+++ index.test.js
@@ -6,6 +6,13 @@ const { execSync } = require('child_process');
 console.log({ platform: process.platform });
 console.log("> git --version");
 execSync("git --version", { stdio: 'inherit' });
+
+console.log("> git config credential.helper");
+execSync("git config credential.helper", { stdio: 'inherit' });
+
+console.log("> git config --unset credential.helper");
+execSync("git config --unset credential.helper", { stdio: 'inherit' });
+
 const { GITHUB_USERNAME } = process.env;
 const githubRepoUrl = `https://github.com/${GITHUB_USERNAME}/webapp-tips-content.git`;
 // const githubRepoUrl = `https://github.com/${GITHUB_USERNAME}/fake-repo.git`;
