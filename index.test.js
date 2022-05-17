aa2db9d3b5294e5316cd467c4ff13dd0acb65c4a
Feranmi Akinlade <30551780+uxFeranmi@users.noreply.github.com>
2022-05-17 10:28:05 +0100

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,11 +1,13 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 const gitAuth = require('./git-auth');
+const { execSync } = require('child_process');
 
 const { GITHUB_USERNAME } = process.env;
-// const githubRepoUrl = `https://github.com/${GITHUB_USERNAME}/webapp-tips-content.git`;
-const githubRepoUrl = `https://github.com/${GITHUB_USERNAME}/fake-repo.git`;
+const githubRepoUrl = `https://github.com/${GITHUB_USERNAME}/webapp-tips-content.git`;
+// const githubRepoUrl = `https://github.com/${GITHUB_USERNAME}/fake-repo.git`;
 gitAuth(`git clone --progress --single-branch ${githubRepoUrl}`)
+	.then(() => execSync("ls webapp-tips-content", { stdio: 'inherit' }))
 	.catch(() => {});
 // gitAuth(`git --version`);
 // gitAuth(`which git`);
